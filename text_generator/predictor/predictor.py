b10c968172cafa9a8204d3dfde76862220b1ec41
Samuel ROCHETTE <srochette@octo.com>
2018-06-11 18:31:39 +0200

    add sampling part in predict

--- text_generator/predictor/predictor.py
+++ text_generator/predictor/predictor.py
@@ -13,6 +13,21 @@ def predict(model, text_starter, prediction_length, character_list_in_train_text
     return prediction
 
 
+# # Sampling dans la distribution de probabilités prédite
+# def sample(preds, temperature=1.0, do_sample=True):
+#     # Avec sampling
+#     preds = np.asarray(preds).astype('float64')
+#     preds = np.log(preds) / temperature
+#     exp_preds = np.exp(preds)
+#     preds = exp_preds / np.sum(exp_preds)
+#     probas = np.random.multinomial(1, preds, 1)
+#     # Sans sampling
+#     preds = np.reshape(preds, (1, preds.shape[0]))
+#     if do_sample:
+#         return np.argmax(probas)
+#     else:
+#         return np.argmax(preds)
+
 def _predict_single_character(model, text_starter, character_list_in_train_text):
     one_hot_encoded_character_sequence = data_processor.get_sequence_of_one_hot_encoded_character(
         text_starter,
