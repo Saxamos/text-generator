f1b4ea973039261b0bde73d0da7f7011a18b6694
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-07-03 09:57:52 +0200

    mob refactoring June 29: refactor: dependency injection (inject character_prediction into text_prediction)

--- text_generator/prediction/prediction.py
+++ text_generator/prediction/prediction.py
@@ -1,5 +1,6 @@
+from prediction.character_prediction import CharacterPrediction
+from prediction.text_prediction import TextPrediction
 from text_generator.neural_network import neural_network
-from text_generator.predictor import predictor
 from text_generator.text_sanitizer import text_sanitizer
 
 
@@ -7,6 +8,7 @@ def predict_text(trained_model_path, text_starter, prediction_length, input_text
     text_sanitizer.sanitize_input_text(input_text_path, sanitized_text_path)
     training_data, character_list_in_training_data = text_sanitizer.read_training_data(sanitized_text_path)
     model = neural_network.load_pre_trained_model(trained_model_path)
+    predictor = TextPrediction(CharacterPrediction())
     prediction = predictor.predict(
         model,
         text_starter,
---
+++ text_generator/prediction/text_prediction.py
@@ -0,0 +1,15 @@
+from prediction.character_prediction import CharacterPrediction
+
+
+class TextPrediction(object):
+    def __init__(self, character_prediction: CharacterPrediction) -> None:
+        self.character_prediction = character_prediction
+
+    def predict(self, model, text_starter, prediction_length, character_list_in_train_text):
+        first_sequence = text_starter
+        prediction = first_sequence
+        for i in range(prediction_length):
+            next_char = self.character_prediction.predict(model, first_sequence, character_list_in_train_text)
+            prediction += next_char
+            first_sequence = first_sequence[1:] + next_char
+        return prediction
