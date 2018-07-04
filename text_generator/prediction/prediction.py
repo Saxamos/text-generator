cbcffdea7b9bef36e497a9c090b7b054052882b5
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-07-04 19:27:00 +0200

    refactor: Inversion of control in prediction by dependency injection (inject training_file_reader, trained_model_loader and text_prediction into prediction)

--- text_generator/prediction/prediction.py
+++ text_generator/prediction/prediction.py
@@ -1,35 +1,18 @@
-import re
+class Prediction(object):
 
-from unidecode import unidecode
+    def __init__(self, training_file_reader, training_model_loader, text_prediction) -> None:
+        self.training_file_reader = training_file_reader
+        self.training_model_loader = training_model_loader
+        self.text_prediction = text_prediction
 
-from prediction.character_prediction import CharacterPrediction
-from prediction.text_prediction import TextPrediction
-from text_generator.neural_network import neural_network
-
-
-def predict_text(trained_model_path, training_input_text_path, text_starter, prediction_length):
-    character_list_in_training_data = extract_all_characters_in_training_input(training_input_text_path)
-    model = neural_network.load_pre_trained_model(trained_model_path)
-    predictor = TextPrediction(CharacterPrediction())
-    prediction = predictor.predict(
-        model,
-        text_starter,
-        prediction_length,
-        character_list_in_training_data
-    )
-    return prediction
-
-
-def extract_all_characters_in_training_input(input_text_path) -> [str]:
-    input_text = open(input_text_path).read()
-    training_input = _sanitize(input_text)
-
-    unique_characters_in_training_input = set(training_input)
-    character_list_in_training_data = sorted(unique_characters_in_training_input)
-
-    return character_list_in_training_data
-
-
-def _sanitize(line):
-    lowered_text = unidecode(line.lower())
-    return re.sub(' ;', ',', lowered_text)
+    def predict_text(self, trained_model_path, training_input_text_path, text_starter, prediction_length):
+        character_list_in_training_data = self.training_file_reader.extract_all_valid_characters_in_file(
+            training_input_text_path)
+        model = self.training_model_loader.load_pre_trained_model(trained_model_path)
+        prediction = self.text_prediction.predict(
+            model,
+            text_starter,
+            prediction_length,
+            character_list_in_training_data
+        )
+        return prediction
