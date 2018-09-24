c4cdab9bb76a3e03ada978bf60fc794a57726380
Samuel <samuel.rochette@yahoo.fr>
2018-09-24 13:09:36 +0200

    refactor: remove unused dimension in prediction

--- text_generator/prediction/prediction.py
+++ text_generator/prediction/prediction.py
@@ -14,26 +14,21 @@ def load_model_and_predict_text(data_dir_name, text_starter, prediction_length, 
     with open(character_list_in_training_data_path) as json_data:
         character_list_in_training_data = json.load(json_data)
 
-    # preprocess text_starter
-    # TODO: virer la dimension en plus en dehors de keras predict
-    encoded_text_starter_sequence = np.array([character_list_in_training_data.index(char) for char in text_starter])
-    shape_with_batch = (1,) + encoded_text_starter_sequence.shape
-    updated_one_hot_encoded_character_sequence = np.reshape(encoded_text_starter_sequence, shape_with_batch)
-
     # load model
     trained_model_path = os.path.join(ROOT_DIR, 'models', data_dir_name, 'model.hdf5')
     model = load_model(trained_model_path)
 
+    # preprocess text_starter
+    encoded_text_starter_sequence = np.array([character_list_in_training_data.index(char) for char in text_starter])
+
     # loop: predict & proba to int
     for i in range(prediction_length):
-        proba_by_character_array = model.predict(updated_one_hot_encoded_character_sequence[:, i:], verbose=0)[0]
+        proba_by_character_array = model.predict(np.expand_dims(encoded_text_starter_sequence[i:], axis=0))[0]
         next_char_index = _sample(proba_by_character_array, temperature)
-        updated_one_hot_encoded_character_sequence = np.append(updated_one_hot_encoded_character_sequence,
-                                                               [[next_char_index]], axis=1)
+        encoded_text_starter_sequence = np.append(encoded_text_starter_sequence, next_char_index)
 
     # postprocess data
-    prediction = [character_list_in_training_data[index] for index in
-                  np.ravel(updated_one_hot_encoded_character_sequence)]
+    prediction = [character_list_in_training_data[index] for index in encoded_text_starter_sequence]
     print(prediction)
     return ''.join(prediction)
 
